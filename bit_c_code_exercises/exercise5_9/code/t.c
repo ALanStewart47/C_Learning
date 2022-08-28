42aa0e2b441eab2a30a68e45eb01649c30716260
ALanStewart <843530433@qq.com>
2022-08-29 00:07:55 +0800

    ~~~

--- bit_c_code_exercises/exercise5_9/code/t.c
+++ bit_c_code_exercises/exercise5_9/code/t.c
@@ -10,34 +10,63 @@
 //
 //	return 0;
 //}
+// 
+// 
 
-struct Book
-{
-	char name[20];
-	char id[20];
-	int price;
-}book1;
+/**********STRUCT*****************/
+//struct Book
+//{
+//	char name[20];
+//	char id[20];
+//	int price;
+//}book1;
+//
+//int main()
+//{
+//	int num = 10;
+//	struct Book b = {"CÓïÑÔ","C20220828",55};
+//	struct Book* pd = &b;
+//
+//	printf("Book name:%s\n",pd->name);
+//	printf("ID number:%s\n", pd->id);
+//	printf("cost:%d\n",pd->price);
+//
+//	printf("%s\n", b.name);
+//	printf("%s\n",b.id);
+//	printf("%d\n",b.price);
+//
+//	printf("%s\n", (*pd).name);
+//	printf("%s\n",(*pd).id);
+//	printf("%d\n", (*pd).price);
+//
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//
+//	int c = a + b * 7;
+//	return 0;
+//
+//}
 
 int main()
 {
-	int num = 10;
-	struct Book b = {"CÓïÑÔ","C20220828",55};
-	struct Book* pd = &b;
+	char a = 3;
+	//0000 0011
+	char b = 127;
+	//01111111
 
-	printf("Book name:%s\n",pd->name);
-	printf("ID number:%s\n", pd->id);
-	printf("cost:%d\n",pd->price);
+	char c = 0;
+	c = a + b;
+	//0000 0011
+	//0111 1111
+	//
 
-	printf("%s\n", b.name);
-	printf("%s\n",b.id);
-	printf("%d\n",b.price);
-
-	printf("%s\n", (*pd).name);
-	printf("%s\n",(*pd).id);
-	printf("%d\n", (*pd).price);
-
+	printf("%d\n", c);
 
 	return 0;
-
 }
-
